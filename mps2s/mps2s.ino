dff7f56a5d2352ea0a29fdb19c7d90863e038d68
Alexey R <lexa260011@ngs.ru>
2018-08-08 03:12:33 +0700

    lcd to slave_i2c

--- mps2s/mps2s.ino
+++ mps2s/mps2s.ino
@@ -1,9 +1,14 @@
 #include <MPS.h>
 
-#define CLK_PIN_I2C P_D13
+#define CLK_PIN_I2C P_D11
 #define DATA_PIN_I2C P_D12
 #define SIGNAL_PIN_I2C P_D2
 #define TRANSMIT_PIN_I2C P_D3
+#define E_PIN_LCD P_D10
+#define RS_PIN_LCD P_D9
+#define DATA_PIN_SH P_D8
+#define CLK_PIN_SH P_D7
+#define LATCH_PIN_SH P_D6
 #define RANGE 50
 
 char master_msg[10] = "Question?";
@@ -21,12 +26,12 @@ void master_i2c() {
   int reading = 0;
   char rbuf[256] = "";
   char tmp = 0;
-  pMode(SIGNAL_PIN_I2C, OUTPUT);
+  pMode(CLK_PIN_I2C, OUTPUT);
+  
   dWrite(SIGNAL_PIN_I2C, HIGH);
   dWrite(CLK_PIN_I2C, LOW);
   dWrite(DATA_PIN_I2C, HIGH);
-  pMode(DATA_PIN_I2C, INPUT_PULLUP);
-  pMode(CLK_PIN_I2C, OUTPUT);
+  
   while (1) {
     if (dRead(TRANSMIT_PIN_I2C) == HIGH && recive_msg == 0 && transmit_msg == 0) {
       Serial.println("Sending");
@@ -62,12 +67,16 @@ void master_i2c() {
           Serial.print(" ");
         } else { 
           index_char = index_char + 1;
-          Serial.println(master_msg[index_char - 1]);
+          Serial.print(master_msg[index_char - 1]);
+          Serial.print("(");
+          Serial.print((int)master_msg[index_char - 1]);
+          Serial.println(")");
           if (master_msg[index_char - 1] == '\0') {
             index_char = 0;
             transmit_msg = 0;
           }
           index_bit = 0;
+          //Send stop-bit
           dWrite(DATA_PIN_I2C, HIGH);
           pMode(DATA_PIN_I2C, INPUT_PULLUP);
           stop_bit = 0;
@@ -90,16 +99,25 @@ void master_i2c() {
           Serial.print(dRead(DATA_PIN_I2C));
           Serial.print(" ");
         } else {
-          rbuf[index_char] = tmp;
-          index_char = index_char + 1;
-          if (tmp == '\0') {
-            index_char = 0;
-            recive_msg = 0;
+          stop_bit = dRead(DATA_PIN_I2C);
+          if (stop_bit == HIGH) {
+            rbuf[index_char] = tmp;
+            index_char = index_char + 1;
+            if (tmp == 0 || tmp == 255) {
+              index_char = 0;
+              recive_msg = 0;
+            }
+            Serial.print(tmp);
+            Serial.print("(");
+            Serial.print((int)tmp);
+            Serial.println(")");
+            tmp = 0;
+            index_bit = 0;
+            recive = 0;
+          } else {
+            Serial.println("Error stop-bit recive. Rewrite last bit");
+            index_bit--;
           }
-          Serial.println(tmp);
-          tmp = 0;
-          index_bit = 0;
-          recive = 0;
         }
       }
     }
@@ -125,17 +143,17 @@ void slave_i2c() {
   char rbuf[256] = "";
   char tmp = '\0';
   
-  pMode(SIGNAL_PIN_I2C, OUTPUT);
   dWrite(SIGNAL_PIN_I2C, LOW);
-  pMode(CLK_PIN_I2C, INPUT);
-  pMode(DATA_PIN_I2C, INPUT_PULLUP);
+
   while (1) {
     if (dRead(TRANSMIT_PIN_I2C) == HIGH && recive_msg == 0 && transmit_msg == 0) {
       Serial.println("Sending");
+      lcd1602_sendstr("Sending", 7, 1, 0);
       transmit = 1;
       transmit_msg = 1;
     } else if (dRead(DATA_PIN_I2C) == LOW && dRead(CLK_PIN_I2C) == HIGH && recive_msg == 0 && transmit_msg == 0) {
       Serial.println("Reciving");
+      lcd1602_sendstr("Reciving", 8, 1, 0);
       tmp = 0;
       recive = 1;
       recive_msg = 1;
@@ -164,11 +182,15 @@ void slave_i2c() {
           Serial.print(" ");
         } else { 
           index_char = index_char + 1;
-          Serial.println(slave_msg[index_char - 1]);
+          Serial.print(slave_msg[index_char - 1]);
+          Serial.print("(");
+          Serial.print((int)slave_msg[index_char - 1]);
+          Serial.println(")");
           if (slave_msg[index_char - 1] == '\0') {
             index_char = 0;
             transmit_msg = 0;
-          }
+          } else 
+            lcd1602_sendstr(&slave_msg[index_char - 1], 1, 2, index_char);
           index_bit = 0;
           dWrite(DATA_PIN_I2C, HIGH);
           pMode(DATA_PIN_I2C, INPUT_PULLUP);
@@ -191,16 +213,27 @@ void slave_i2c() {
           Serial.print(dRead(DATA_PIN_I2C));
           Serial.print(" ");
         } else {
-          rbuf[index_char] = tmp;
-          index_char = index_char + 1;
-          if (tmp == '\0') {
-            index_char = 0;
-            recive_msg = 0;
+          stop_bit = dRead(DATA_PIN_I2C);
+          if (stop_bit == HIGH) {
+            rbuf[index_char] = tmp;
+            index_char = index_char + 1;
+            if (tmp == 0 || tmp == 255) {
+              index_char = 0;
+              recive_msg = 0;
+            }
+            Serial.print(tmp);
+            Serial.print("(");
+            Serial.print((int)tmp);
+            Serial.println(")"); 
+            if (tmp != '\0')
+              lcd1602_sendstr(&rbuf[index_char - 1], 1, 2, index_char);
+            tmp = 0;
+            index_bit = 0;
+            recive = 0;
+          } else {
+            Serial.println("Error stop-bit recive. Rewrite last bit");
+            index_bit--;
           }
-          Serial.println(tmp);
-          tmp = 0;
-          index_bit = 0;
-          recive = 0;
         }
       }
       if (dRead(CLK_PIN_I2C) == LOW && reading == 1) reading = 0;
@@ -209,10 +242,91 @@ void slave_i2c() {
   }
 }
 
+void data_shift595(byte data) {
+  shiftOut(DATA_PIN_SH, CLK_PIN_SH, MSBFIRST, data);
+
+  digitalWrite(LATCH_PIN_SH, LOW);
+  digitalWrite(LATCH_PIN_SH, HIGH);
+  digitalWrite(LATCH_PIN_SH, LOW);
+}
+
+void lcd1602_sendcommand(byte command) {
+  dWrite(RS_PIN_LCD, LOW);
+  data_shift595(command);
+  lcd1602_pulse();
+  delayMicroseconds(100);
+}
+
+void lcd1602_senddata(byte data) {
+  dWrite(RS_PIN_LCD, HIGH);
+  data_shift595(data);
+  lcd1602_pulse();
+  delayMicroseconds(100);
+}
+
+void lcd1602_sendstr(char* str, int len, int strn, int pos) {
+  int i = 0;
+  if (pos + len >= 16) return;
+  if (strn == 1)
+    lcd1602_sendcommand(LCD_COMMAND_SET_CURSOR(0 + pos));
+  else
+    lcd1602_sendcommand(LCD_COMMAND_SET_CURSOR(63 + pos));
+
+  for (i = 0; i < len; ++i) {
+    lcd1602_senddata(str[i]);
+  }
+  
+  for (i = len; i <16; ++i) lcd1602_senddata(32);
+}
+
+void lcd1602_pulse() {
+  dWrite(E_PIN_LCD, LOW);
+  dWrite(E_PIN_LCD, HIGH);
+  dWrite(E_PIN_LCD, LOW);
+}
+
+void lcd1602_init() {
+  delay(15);
+  lcd1602_sendcommand(LCD_COMMAND_WORK_MODE(WM_8BIT, WM_2LINE,WM_5X8));
+  delay(10);
+  lcd1602_sendcommand(LCD_COMMAND_WORK_MODE(WM_8BIT, WM_2LINE,WM_5X8));
+  delay(10);
+  lcd1602_sendcommand(LCD_COMMAND_WORK_MODE(WM_8BIT, WM_2LINE,WM_5X8));
+  delay(10);
+  lcd1602_sendcommand(LCD_COMMAND_DISPLAY(DS_ON,DS_VISIBLE,DS_NOBLINK));
+  delay(10);
+  lcd1602_sendcommand(LCD_COMMAND_SHIFT_MODE(RIGHT,SH_SHIFT_NO));
+  delay(10);
+  lcd1602_sendcommand(LCD_COMMAND_SHIFT(SH_CURSOR,RIGHT));
+  delay(10);
+  lcd1602_sendcommand(LCD_COMMAND_CLR());
+  delay(10); 
+}
+
 void setup() {
+  //SLAVE I2C
+  pMode(SIGNAL_PIN_I2C, OUTPUT);
+  dWrite(SIGNAL_PIN_I2C, LOW);
+  pMode(CLK_PIN_I2C, INPUT);
+  pMode(DATA_PIN_I2C, INPUT_PULLUP);
+  //
+  
+  pMode(E_PIN_LCD,OUTPUT);
+  pMode(RS_PIN_LCD,OUTPUT);
+  pMode(DATA_PIN_SH,OUTPUT);
+  pMode(CLK_PIN_SH,OUTPUT);
+  pMode(LATCH_PIN_SH,OUTPUT);
+  
+  dWrite(E_PIN_LCD, LOW);
+  dWrite(RS_PIN_LCD, LOW);
+  dWrite(DATA_PIN_SH, LOW);
+  dWrite(CLK_PIN_SH, LOW);
+  dWrite(LATCH_PIN_SH, LOW);
+  
   Serial.begin(9600);
-  master_i2c();
-  //slave_i2c();
+  lcd1602_init();
+  //master_i2c();
+  slave_i2c();
 }
 
 void loop() { 
